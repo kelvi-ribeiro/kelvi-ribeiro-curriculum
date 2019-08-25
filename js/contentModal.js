3a145c363a120dca35c8f411aff8d19384ff6538
kelvi-ribeiro <kelvi.ribeiro@gmail.com>
2019-08-24 22:05:00 -0300

    Criando arquivo de conterá o html de cada content de modal para que mude dinamicamente

---
+++ js/contentModal.js
@@ -0,0 +1,20 @@
+var vestibularUnigranrio =  '<h2>Inscrição de Candidatos de Primeira Gradução - Unigranrio</h2>' +
+'                        <div class="h-50"></div>' +
+'                        <p>Site responsável pela inscrição de Candidatos de primeira graduação da Instituição' +
+'                            Unigranrio.</p>' +
+'                        <p>Suporta mais de 40 Unidades ativas da Unigranrio. O candidato(futuro aluno) escolhe os dados' +
+'                            de sua oferta(Curso, Unidade, Forma de Ingresso, Modalidade, Turno) e logo após isso, o' +
+'                            Sistema envia periodicamente e-mails da situação da inscrição do mesmo.</p>' +
+'                        <br />' +
+'                        <br /> <img src="img/portfolio/vestibular-unigranrio.png" alt="" class="img-responsive" />' +
+'                        <br />' +
+'                        <br />' +
+'                        <p>Sem Dúvidas, esse foi o projeto que eu mais participei, atuei desde o FrontEnd até o BackEnd' +
+'                            desse projeto que possuem grandes demandas até o dia de Hoje(24/08/2019).</p>' +
+'                        <p>Existe também uma integração com o serviço do Hubspot(Serviõ que oferece um conjunto completo' +
+'                            de Software para marteking, vendas e atendimento ao cliente), em quase todos os formulários' +
+'                            do site, os dados do Candidato são enviados através da API do Hubspot</p>' +
+'                        <p>Aproveitando para falar do BackEnd desse site(que eu também participei ativamente), foi feito' +
+'                            com Java Rest API com Spring Boot, usando SGBG SQL Server.</p>' +
+'                        <br />' +
+'                        <br />';
